381ec6e18a4cbe04df73c6cffbd014187c1c35aa
nandakoryaaa <dr_noise@mail.ru>
2021-05-07 00:19:38 +0300

    shaders.

---
+++ src/main_3.c
@@ -0,0 +1,168 @@
+#define SDL_MAIN_HANDLED
+
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+#include "gl.h"
+#include <SDL2/SDL_opengl.h>
+
+char string_buffer[1024];
+char log_buffer[1024];
+
+int main(int argc, char * argv[]) {
+
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	SDL_GL_SetAttribute(
+		SDL_GL_CONTEXT_PROFILE_MASK,
+		SDL_GL_CONTEXT_PROFILE_CORE);
+
+
+	int width = 800;
+	int height = 600;
+
+	SDL_Window * window = SDL_CreateWindow(
+		"OpenGL App",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		width, height,
+		SDL_WINDOW_OPENGL
+	);
+
+	SDL_GLContext context = SDL_GL_CreateContext(window);
+
+	FILE *file = fopen("shaders/vertex_shader.glsl", "r");
+	int size = fread(string_buffer, 1, 1023, file);
+	fclose(file);
+	string_buffer[size] = 0;
+
+	GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	const GLchar* parts[] = {(const GLchar *)string_buffer};
+	glShaderSource(vertex_shader, 1, parts, NULL);
+	glCompileShader(vertex_shader);
+
+	GLint result;
+	glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE) {
+		printf("Vertex shader compilation failed!\n");
+		GLint logLen;
+		glGetShaderiv(vertex_shader, GL_INFO_LOG_LENGTH, &logLen);
+		if (logLen > 0) {
+			GLsizei written;
+    		glGetShaderInfoLog(vertex_shader, sizeof(log_buffer) - 1, &written, log_buffer);
+			log_buffer[logLen] = 0;
+			printf("Shader log:\n%s", log_buffer);
+		}
+		return 1;
+	}
+
+	file = fopen("shaders/fragment_shader.glsl", "r");
+	size = fread(string_buffer, 1, 1023, file);
+	fclose(file);
+	string_buffer[size] = 0;
+
+	GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	//const GLchar* parts[] = {(const GLchar *)string_buffer};
+	glShaderSource(fragment_shader, 1, parts, NULL);
+	glCompileShader(fragment_shader);
+
+//	GLint result;
+	glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE) {
+		printf("Fragment shader compilation failed!\n");
+		GLint logLen;
+		glGetShaderiv(fragment_shader, GL_INFO_LOG_LENGTH, &logLen);
+		if (logLen > 0) {
+			GLsizei written;
+    		glGetShaderInfoLog(fragment_shader, sizeof(log_buffer) - 1, &written, log_buffer);
+			log_buffer[logLen] = 0;
+			printf("Shader log:\n%s", log_buffer);
+		}
+		return 1;
+	}
+
+	GLuint program = glCreateProgram();
+	glAttachShader(program, vertex_shader);
+	glAttachShader(program, fragment_shader);
+	glLinkProgram(program);
+
+	glGetProgramiv(program, GL_LINK_STATUS, &result);
+	if (result == GL_FALSE) {
+		printf("Failed to link shader program\n");
+		GLint logLen;
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &logLen);
+		if (logLen > 0) {
+			GLsizei written;
+			glGetProgramInfoLog(program, sizeof(log_buffer) - 1, &written, log_buffer);
+			printf("Program log: \n%s", log_buffer);
+		}
+		return 1;
+	}
+
+	glUseProgram(program);
+
+	glClearColor(0.5, 0.0, 0.0, 0.0);
+	glViewport(0, 0, width, height);
+
+	float vertices[] = {
+		-0.8f, -0.8f, 0.0f,
+		0.8f, -0.8f, 0.0f,
+		0.0f,  0.8f, 0.0f
+	};
+
+
+	GLuint vertex_buffer_handle;
+	glGenBuffers(1, &vertex_buffer_handle);
+
+
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_handle);
+	glBufferData(
+		GL_ARRAY_BUFFER, 9 * sizeof(float), vertices,
+		GL_STATIC_DRAW
+	);
+
+	GLuint vertex_array_handle;
+
+	glGenVertexArrays(1, &vertex_array_handle);
+	glBindVertexArray(vertex_array_handle);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_handle);
+	glVertexAttribPointer(
+		0, 3,
+		GL_FLOAT, GL_FALSE,
+		0, NULL
+	);
+
+
+	SDL_Event event;
+
+	for (;;) {
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBindVertexArray(vertex_array_handle);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		if (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				break;
+			}
+		}
+
+		SDL_GL_SwapWindow(window);
+		SDL_Delay(1);
+	}
+
+	SDL_GL_DeleteContext(context);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
+}
